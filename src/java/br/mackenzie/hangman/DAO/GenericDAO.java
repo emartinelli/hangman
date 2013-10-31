4eea448446af454e43df5edc7a18af7730bcd968
31281354 <31281354@090151-P33S01.macklabs.br>
2013-10-31 18:12:07 -0200

    Generic DAO implementation

--- src/java/br/mackenzie/hangman/DAO/GenericDAO.java
+++ src/java/br/mackenzie/hangman/DAO/GenericDAO.java
@@ -4,10 +4,17 @@
  */
 package br.mackenzie.hangman.DAO;
 
+import br.mackenzie.hangman.exception.PersistenceException; 
+import java.util.List;
+
 /**
  *
  * @author 31281354
  */
-public interface GenericDAO {
-    
+public interface GenericDAO<T> {
+    void inserir(T obj) throws PersistenceException; 
+    void atualizar(T obj) throws PersistenceException; 
+    void deletar(Integer id) throws PersistenceException; 
+    List<T> listarTodos() throws PersistenceException; 
+    T buscarPorId(Integer id) throws PersistenceException; 
 }
