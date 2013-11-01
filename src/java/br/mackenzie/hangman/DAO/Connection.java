a4546b5e5b1544a49727acd8db1fd0a3e7693dc9
31223567 <31223567@090138-P33S01.macklabs.br>
2013-11-01 17:09:34 -0200

    Class Connection

---
+++ src/java/br/mackenzie/hangman/DAO/Connection.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.mackenzie.hangman.DAO;
+
+/**
+ *
+ * @author 31223567
+ */
+public class Connection {
+    
+}
