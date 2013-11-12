d691e8ca5c4e0f46c2d97e878415f9e10b7950ca
User <User@User-VAIO>
2013-11-12 00:04:44 -0200

    Inclusao da função retornaID na PlayerDAO

--- src/java/br/mackenzie/hangman/DAO/PlayerDAO.java
+++ src/java/br/mackenzie/hangman/DAO/PlayerDAO.java
@@ -129,6 +129,28 @@ public class PlayerDAO implements GenericDAO<Player>{
         }
         return player;
     }
+    
+    public int retornaId (Player player) throws PersistenceException {
+        Connection connection = null;
+        int id;
+        try {
+            connection = ConnectionHangman.getInstance().getConnection();
+            
+            String sql = "SELECT * FROM HANGMAN_DB.PLAYER WHERE NICKNAME = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, player.getNickname());
+            ResultSet result = preparedStatement.executeQuery();
+            id = result.getInt("IDPLAYER");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(br.mackenzie.hangman.DAO.PlayerDAO.class.getName()).log(Level.SEVERE, null, ex);
+            throw new PersistenceException("Não foi possível burcar!");
+        }
+        catch ( SQLException ex) {
+            Logger.getLogger(br.mackenzie.hangman.DAO.PlayerDAO.class.getName()).log(Level.SEVERE, null, ex);
+            throw new PersistenceException("Não foi possível burcar!");
+        }
+        return id;
+    }
 
     @Override
     public void deletar(Integer id) throws PersistenceException {
