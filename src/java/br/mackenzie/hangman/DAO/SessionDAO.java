fc0eed478f55a22ea0baccb91e787b4bd29257e7
User <User@User-VAIO>
2013-11-12 00:04:17 -0200

    Melhoria na SessionDAO (Em Progresso)

--- src/java/br/mackenzie/hangman/DAO/SessionDAO.java
+++ src/java/br/mackenzie/hangman/DAO/SessionDAO.java
@@ -17,13 +17,47 @@ import java.util.logging.Logger;
 public class SessionDAO implements GenericDAO<Session>{
 
     @Override
-    public void inserir(Session obj) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void inserir(Session session) throws PersistenceException {
+        Connection connection = null;
+        try {
+            connection = ConnectionHangman.getInstance().getConnection();
+            String sql = "INSERT INTO HANGMAN_DB.SESSION (TIME,SCORE,IDPLAYER,IDWORD) VALUES (?,?,?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, session.getTime());
+            preparedStatement.setInt(2,session.getScore());
+            preparedStatement.setInt(2,new PlayerDAO().retornaId(session.getPlayer()));
+            preparedStatement.setInt(2,new WordDAO().retornaId(session.getWord()));
+            preparedStatement.executeUpdate();
+            connection.close();
+
+        }catch (ClassNotFoundException ex) {
+            Logger.getLogger(br.mackenzie.hangman.DAO.SessionDAO.class.getName()).log(Level.SEVERE, null, ex);
+            throw new PersistenceException("Inserção não realizada!");
+        }
+        catch ( SQLException ex) {
+            Logger.getLogger(br.mackenzie.hangman.DAO.SessionDAO.class.getName()).log(Level.SEVERE, null, ex);
+            throw new PersistenceException("Inserção não realizada!");
+        }
     }
 
     @Override
-    public void atualizar(Session obj) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void atualizar(Session session) throws PersistenceException {
+        Connection connection = null;
+        try {
+            connection = ConnectionHangman.getInstance().getConnection();
+            String sql = "UPDATE HAGMAN_DB.SESSION WHERE IDSESSION = ?  ";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.executeUpdate();
+            connection.close();
+
+        }catch (ClassNotFoundException ex) {
+            Logger.getLogger(br.mackenzie.hangman.DAO.SessionDAO.class.getName()).log(Level.SEVERE, null, ex);
+            throw new PersistenceException("Atualização não realizada!");
+        }
+        catch ( SQLException ex) {
+            Logger.getLogger(br.mackenzie.hangman.DAO.SessionDAO.class.getName()).log(Level.SEVERE, null, ex);
+            throw new PersistenceException("Atualização não realizada!");
+        }
     }
 
     @Override
