7574766e193b469bfb921863e2e28f28366ecbcc
Elvio Martinelli <elviomartinelli@gmail.com>
2013-11-12 05:50:11 -0200

    Update hangman_db_20131021.sql

--- bd/scripts/hangman_db_20131021.sql
+++ bd/scripts/hangman_db_20131021.sql
@@ -1,26 +1,29 @@
 CREATE TABLE hangman_db.player (
-  idplayer INT,
-  nickname VARCHAR(45),
-  password VARCHAR(45),
-  isAdmin INT,
+  idplayer INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+  nickname VARCHAR(45) NOT NULL,
+  password VARCHAR(45) NOT NULL,
+  isAdmin INTEGER,
   PRIMARY KEY (idplayer) );
-
-CREATE  TABLE hangman_db.session (
-  idSession INT,
-  time INT ,
-  score INT ,
-  idPlayer INT ,
-  idWord INT ,
-  PRIMARY KEY (idSession) );
-
+  
 CREATE  TABLE hangman_db.word (
-  idword INT,
-  word VARCHAR(45) ,
+  idword INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+  realWord VARCHAR(45) NOT NULL,
   errorFrequency VARCHAR(45) ,
   PRIMARY KEY (idword) );
 
+CREATE  TABLE hangman_db.session (
+  idSession INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+  timeSession INTEGER,
+  score INTEGER NOT NULL,
+  idPlayer INTEGER NOT NULL,
+  idWord INTEGER NOT NULL,
+  FOREIGN KEY (idPlayer) REFERENCES hangman_db.player (idPlayer) ON DELETE CASCADE,
+  FOREIGN KEY (idWord) REFERENCES hangman_db.word (idWord) ON DELETE CASCADE,
+  PRIMARY KEY (idSession) );
+
 CREATE  TABLE hangman_db.tip (
-  idtip INT,
-  information VARCHAR(45) ,
-  idWord INT ,
+  idtip INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+  information VARCHAR(45),
+  idWord INTEGER,
+  FOREIGN KEY (idWord) REFERENCES hangman_db.word (idWord) ON DELETE CASCADE,
   PRIMARY KEY (idtip) );
